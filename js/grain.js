be7a940e8c977d6e4ae0c92bfc4585698a746e24
Manaut <manaut.bertonboitel@outlook.fr>
2022-03-23 16:35:48 +0100

    Update grain

--- js/grain.js
+++ js/grain.js
@@ -2,8 +2,8 @@ var options = {
     "animate": true,
   "patternWidth": 500,
   "patternHeight": 500,
-  "grainOpacity": 0.35,
-  "grainDensity": 5,
+  "grainOpacity": 0.4,
+  "grainDensity": 2.5,
   "grainWidth": 1,
   "grainHeight": 1
 };
