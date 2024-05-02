4ee1cf09190b81a545499d09c3d4903a6ad3ec9b
Manaut <manaut.bertonboitel@outlook.fr>
2024-05-02 23:49:12 +0200

    noise update

--- js/cursor.js
+++ js/cursor.js
@@ -52,3 +52,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
       initCursor = false;
     };
 });
+
+const box = document.querySelector("body");
+const pageX = document.getElementById("x");
+const pageY = document.getElementById("y");
+
+function updateDisplay(event) {
+  pageX.innerText = event.pageX;
+  pageY.innerText = event.pageY;
+}
+
+box.addEventListener("mousemove", updateDisplay, false);
+box.addEventListener("mouseenter", updateDisplay, false);
+box.addEventListener("mouseleave", updateDisplay, false);
+box.addEventListener("mouseover", updateDisplay, false);
