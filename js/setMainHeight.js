ead1da1feed227e15e920c4fbc8f2e5d638c615e
Manaut <manaut.bertonboitel@outlook.fr>
2024-05-06 01:02:06 +0200

    next hover category home page

--- js/setMainHeight.js
+++ js/setMainHeight.js
@@ -28,26 +28,33 @@
 var navbar = document.querySelector("nav");
 var footer = document.querySelector("footer");
 var mainSection = document.querySelector("main");
+var cssRoot = document.querySelector(":root");
 
 function reportWindowSize() {
 
-    // var navbarStyle = navbar.currentStyle || window.getComputedStyle(navbar);
-    var navbarStyle = window.getComputedStyle(navbar);
-    var navbarFullHeight = navbar.height + navbarStyle.marginTop;
-    console.log("navbar offset height = " + navbar.height);
-    console.log("navbar margin-top = " + navbarStyle.marginTop);
-    console.log("navbar offset + height = " + navbarFullHeight);
-
-    // var footerStyle = footer.currentStyle || window.getComputedStyle(footer);
-    // var footerFullHeight = footer.offsetHeight + footerStyle.marginTop; 
-
-
-    // var mainSectionStyle = mainSection.currentStyle || window.getComputedStyle(mainSection);
-
-    // mainSectionStyle.offsetHeight = mainSectionStyle.offsetHeight - footerFullHeight - navbarFullHeight;
-    // console.log("main height =" + mainSectionStyle.offsetHeight);
+    // console.log(getComputedStyle(cssRoot).getPropertyValue("--navAndFooterHeight"));
+    // console.log("navbar offset height = " + window.getComputedStyle(navbar).height);
+    // console.log("navbar margin-top = " + window.getComputedStyle(navbar).marginTop);
+    var w = window.innerWidth;
+    if (w > 1070) {
+        var navbarValue = parseFloat(window.getComputedStyle(navbar).height, 10) + parseFloat(window.getComputedStyle(navbar).marginTop, 10) + "px";
+        var footerValue = parseFloat(window.getComputedStyle(footer).height, 10) + parseFloat(window.getComputedStyle(footer).marginBottom, 10) + "px";
+        var mainSectionMargin = parseFloat(window.getComputedStyle(mainSection).marginTop, 10) + parseFloat(window.getComputedStyle(mainSection).marginBottom, 10) + "px";
+        var finalValue = parseFloat(navbarValue, 10) + parseFloat(footerValue, 10) + parseFloat(mainSectionMargin, 10) + "px";
+        console.log("finalValue = " + finalValue);
+    
+        cssRoot.style.setProperty("--navAndFooterHeight", finalValue);
+    } else if (w < 1070) {
+        var footerValue = parseFloat(window.getComputedStyle(footer).height, 10) + parseFloat(window.getComputedStyle(footer).marginBottom, 10) + "px";
+        var mainSectionMargin = parseFloat(window.getComputedStyle(mainSection).marginTop, 10) + parseFloat(window.getComputedStyle(mainSection).marginBottom, 10) + "px";
+        var finalValue = parseFloat(footerValue, 10) + parseFloat(mainSectionMargin, 10) + "px";
+        console.log("finalValue = " + finalValue);
+    
+        cssRoot.style.setProperty("--navAndFooterHeight", finalValue);
+    }
+    // console.log(getComputedStyle(cssRoot).getPropertyValue("--navAndFooterHeight"));
 }
 
-document.addEventListener("DOMContentLoaded", reportWindowSize());
-
+// document.addEventListener("DOMContentLoaded", reportWindowSize());
+window.onload = reportWindowSize;
 window.onresize = reportWindowSize;
